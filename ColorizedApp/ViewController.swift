be8722f65f0ccb870e8786694f510809c94d8c5d
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2022-10-30 21:46:46 +0100

    Colorized App

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorizedView: UIView!
+    
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
+    @IBOutlet var redColorValue: UILabel!
+    @IBOutlet var greenColorValue: UILabel!
+    @IBOutlet var blueColorValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupSlider(slider: redColorSlider, color: .red)
+        setupSlider(slider: greenColorSlider, color: .green)
+        setupSlider(slider: blueColorSlider, color: .blue)
+        
+        
+    
     }
 
+    @IBAction func redSliderAction() {
+        redColorValue.text = redColorSlider.value.formatted()
+    }
+    
+    @IBAction func greenSliderAction() {
+    }
+    
+    @IBAction func bluerSliderAction() {
+    }
+    
+    private func setupSlider(slider: UISlider, color: UIColor) {
+        slider.value = 0
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = color
+    }
+    
 
 }
 
