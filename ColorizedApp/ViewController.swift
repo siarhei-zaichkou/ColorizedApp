b3644132dccd720c575f6c5745218696c75aad8a
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2022-10-31 00:13:09 +0100

    added all functional

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -19,34 +19,55 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorValue: UILabel!
     @IBOutlet var blueColorValue: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupSlider(slider: redColorSlider, color: .red)
-        setupSlider(slider: greenColorSlider, color: .green)
-        setupSlider(slider: blueColorSlider, color: .blue)
+        setupSlider(redColorSlider, color: .red)
+        setupSlider(greenColorSlider, color: .green)
+        setupSlider(blueColorSlider, color: .blue)
         
-        
-    
+        setupColorizedView()
     }
 
     @IBAction func redSliderAction() {
-        redColorValue.text = redColorSlider.value.formatted()
+        redColorValue.text = String(format: "%.2f", redColorSlider.value)
+        colorizedView.backgroundColor = UIColor.init(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value), alpha: 1)
     }
     
     @IBAction func greenSliderAction() {
+        greenColorValue.text = String(format: "%.2f", greenColorSlider.value)
+        colorizedView.backgroundColor = UIColor.init(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value), alpha: 1)
     }
     
     @IBAction func bluerSliderAction() {
+        blueColorValue.text = String(format: "%.2f", blueColorSlider.value)
+        colorizedView.backgroundColor = UIColor.init(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value), alpha: 1)
     }
     
-    private func setupSlider(slider: UISlider, color: UIColor) {
+    private func setupSlider(_ slider: UISlider, color: UIColor) {
         slider.value = 0
         slider.minimumValue = 0
         slider.maximumValue = 1
         slider.minimumTrackTintColor = color
     }
     
+    private func setupColorizedView() {
+        colorizedView.layer.cornerRadius = 15
+        colorizedView.backgroundColor = UIColor.init(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value), alpha: 1)
+    }
 
 }
 
